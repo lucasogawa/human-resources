c2e419343c95aa4153cc2f666e75e6380c4b2285
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-04-11 22:59:39 -0300

    Configuring Eureka Clients

--- payroll-api/src/main/java/com/ogawalucas/payrollapi/Application.java
+++ payroll-api/src/main/java/com/ogawalucas/payrollapi/Application.java
@@ -2,12 +2,12 @@ package com.ogawalucas.payrollapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
-@RibbonClient("worker-api")
 public class Application {
 
 	public static void main(String[] args) {
