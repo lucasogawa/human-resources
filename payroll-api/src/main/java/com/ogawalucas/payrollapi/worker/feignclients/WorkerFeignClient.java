3e74f9d1815725374006ac385e47c3a05e4260bf
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-04-11 22:09:48 -0300

    Adding Ribbon

--- payroll-api/src/main/java/com/ogawalucas/payrollapi/worker/feignclients/WorkerFeignClient.java
+++ payroll-api/src/main/java/com/ogawalucas/payrollapi/worker/feignclients/WorkerFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "worker-api", path = "/workers")
 public interface WorkerFeignClient {
 
     @GetMapping("{id}")
--- worker-api/src/main/java/com/ogawalucas/workerapi/worker/resources/WorkerResource.java
+++ worker-api/src/main/java/com/ogawalucas/workerapi/worker/resources/WorkerResource.java
@@ -3,7 +3,9 @@ package com.ogawalucas.workerapi.worker.resources;
 import com.ogawalucas.workerapi.commons.expections.NotFoundException;
 import com.ogawalucas.workerapi.worker.entities.Worker;
 import com.ogawalucas.workerapi.worker.repositories.WorkerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("workers")
 public class WorkerResource {
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository repository;
@@ -25,6 +31,8 @@ public class WorkerResource {
 
     @GetMapping("{id}")
     public Worker findById(@PathVariable Long id) {
+        log.info("Port = " + env.getProperty("local.server.port"));
+
         return repository.findById(id)
             .orElseThrow(() -> new NotFoundException("Worker"));
     }
