f2a33ca28c1bd2d6ebe47131c43754f0b271cdbe
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-04-26 21:36:08 -0300

    Adding Actuator for Runtime Config Refresh

--- worker-api/src/main/java/com/ogawalucas/workerapi/worker/resources/WorkerResource.java
+++ worker-api/src/main/java/com/ogawalucas/workerapi/worker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import com.ogawalucas.workerapi.worker.repositories.WorkerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("workers")
 public class WorkerResource {
