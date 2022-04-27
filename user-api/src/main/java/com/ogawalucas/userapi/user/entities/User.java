ef1b7bc7becf3b3a49e66dbf26f73a815b4d4449
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-04-26 22:13:16 -0300

    Adding User, Role and Association

---
+++ user-api/src/main/java/com/ogawalucas/userapi/user/entities/User.java
@@ -0,0 +1,34 @@
+package com.ogawalucas.userapi.user.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "USER")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "USER_ROLE",
+        joinColumns = @JoinColumn(name = "USER_ID"),
+        inverseJoinColumns = @JoinColumn(name = "ROLE_ID")
+    )
+    private Set<Role> roles = new HashSet<>();
+}
