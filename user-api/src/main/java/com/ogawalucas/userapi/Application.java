6910bf905b6c788790b117de8952701a60f441db
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-04-26 21:59:26 -0300

    Configuring user-api

--- user-api/src/main/java/com/ogawalucas/userapi/Application.java
+++ user-api/src/main/java/com/ogawalucas/userapi/Application.java
@@ -2,12 +2,13 @@ package com.ogawalucas.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class UserApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
