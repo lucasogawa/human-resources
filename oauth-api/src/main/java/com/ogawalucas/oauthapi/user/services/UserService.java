532ff13180cc917303bab4ab5f25777cb564e53e
Lucas Akio Ogawa <lucas.ogawa@platosedu.com.br>
2022-09-13 17:15:18 -0300

    Adding Authorization and JWT

--- oauth-api/src/main/java/com/ogawalucas/oauthapi/user/services/UserService.java
+++ oauth-api/src/main/java/com/ogawalucas/oauthapi/user/services/UserService.java
@@ -3,15 +3,23 @@ package com.ogawalucas.oauthapi.user.services;
 import com.ogawalucas.oauthapi.user.entities.User;
 import com.ogawalucas.oauthapi.user.feignclients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient feignClient;
 
     public User findAll(String email) {
         return feignClient.findAll(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return feignClient.findAll(username);
     }
 }
