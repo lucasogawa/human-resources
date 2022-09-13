c1cf2d9b4be3c87d3a9335929834ccd9ccd6c5e9
Lucas Akio Ogawa <lucas.ogawa@platosedu.com.br>
2022-09-13 17:31:52 -0300

    Adding Zuul Authorization

---
+++ gateway-zuul-api/src/main/java/com/ogawalucas/gatewayzuulapi/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.ogawalucas.gatewayzuulapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        var tokenConverter = new JwtAccessTokenConverter();
+
+        tokenConverter.setSigningKey("MY-SECRET-KEY");
+
+        return tokenConverter;
+    }
+}
